ecfda3e71e0f1699d5fdcea693bcf3fd4ac94b85
Naman Arora <aroranaman17@outlook.com>
2020-08-08 20:53:00 +0530

    Default to int64 in all cases

--- swaggerGen.js
+++ swaggerGen.js
@@ -60,9 +60,7 @@ function convert() {
 
 	if (num % 1 === 0 && !document.getElementById("noInt").checked) {
         outSwagger += indentator + '"type": "integer",';
-        if (num < 2147483647 && num > -2147483647) {
-          outSwagger += indentator + '"format": "int32"';
-        } else if (Number.isSafeInteger(num)) {
+        if (Number.isSafeInteger(num)) {
           outSwagger += indentator + '"format": "int64"';
         } else {
           outSwagger += indentator + '"format": "unsafe"';
