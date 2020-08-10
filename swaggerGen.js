60630c3e958963d0260e991d264bdc186c152235
Naman Arora <aroranaman17@outlook.com>
2020-08-10 18:25:56 +0530

    Do not use date-time format in date strings

--- swaggerGen.js
+++ swaggerGen.js
@@ -42,7 +42,7 @@ function convert() {
     } else if (typeof obj === "object") { //attribute is an object
       convertObject(obj);
     } else if (typeof obj === "string") { //attribute is a string
-      convertString(obj);
+      outSwagger += indentator + '"type": "string"';
     } else if (typeof obj === "boolean") { // attribute is a boolean
       outSwagger += indentator + '"type": "boolean"';
     } else { // not a valid Swagger type
@@ -75,28 +75,28 @@ function convert() {
   };
 
   //date is ISO8601 format - https://xml2rfc.tools.ietf.org/public/rfc/html/rfc3339.html#anchor14
-  function convertString (str) {
-	/* 
-    Append to 'outSwagger' string with Swagger schema attributes relative to given string
-    Global variables updated: 
-    -outSwagger
-    */
+//   function convertString (str) {
+// 	/* 
+//     Append to 'outSwagger' string with Swagger schema attributes relative to given string
+//     Global variables updated: 
+//     -outSwagger
+//     */
 
-    let regxDate = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
-    regxDateTime = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]).([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]{1,2})?(Z|(\+|\-)([0-1][0-9]|2[0-3]):[0-5][0-9])$/;
+//     let regxDate = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
+//     regxDateTime = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]).([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]{1,2})?(Z|(\+|\-)([0-1][0-9]|2[0-3]):[0-5][0-9])$/;
     
-	outSwagger += indentator + '"type": "string"';
-	if (regxDateTime.test(str)) {
-        outSwagger += ','
-        outSwagger += indentator + '"format": "date-time"';
-    } else if (regxDate.test(str)) {
-        outSwagger += ','
-        outSwagger += indentator + '"format": "date"';
-    }
-	if (document.getElementById("requestExamples").checked) { //Log example if checkbox is checked 
-        outSwagger += "," + indentator + '"example": "' + str + '"';
-    }
-  };
+// 	outSwagger += indentator + '"type": "string"';
+// 	if (regxDateTime.test(str)) {
+//         outSwagger += ','
+//         outSwagger += indentator + '"format": "date-time"';
+//     } else if (regxDate.test(str)) {
+//         outSwagger += ','
+//         outSwagger += indentator + '"format": "date"';
+//     }
+// 	if (document.getElementById("requestExamples").checked) { //Log example if checkbox is checked 
+//         outSwagger += "," + indentator + '"example": "' + str + '"';
+//     }
+//   };
 
   function convertArray (obj) {
 	/* 
